672a505d937661c8af04a487e068690d82e083b3
muyi <muyi@gegejia.com>
2019-11-01 16:33:53 +0800

    代码微调

--- mtrace-ggj-agent/src/main/java/com/ggj/tester/MethodAdapter.java
+++ mtrace-ggj-agent/src/main/java/com/ggj/tester/MethodAdapter.java
@@ -12,7 +12,7 @@ public class MethodAdapter extends MethodVisitor implements Opcodes {
     }
 
     @Override
-    public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+    public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
         if (!ClassTransformer.filterBySelf(owner) && !name.equals("<init>") && !name.equals("clinit") && traceMethod.equals("true")) {
 
             // System.err.println("thread id = " + Thread.currentThread().getId() + ",call method = " + owner + "." + name);
@@ -40,7 +40,7 @@ public class MethodAdapter extends MethodVisitor implements Opcodes {
             mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
 
             // 方法调用
-            mv.visitMethodInsn(opcode, owner, name, desc, itf);
+            mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
 
             mv.visitFieldInsn(GETSTATIC, "java/lang/System", "err", "Ljava/io/PrintStream;");
             mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
@@ -66,8 +66,7 @@ public class MethodAdapter extends MethodVisitor implements Opcodes {
             mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
 
         } else {
-            mv.visitMethodInsn(opcode, owner, name, desc, itf);
-
+            mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
         }
     }
 }
