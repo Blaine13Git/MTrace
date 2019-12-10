1cc7a9e93e2eb9799010f7852d7a231919d7b2ce
muyi <muyi@gegejia.com>
2019-12-10 14:01:46 +0800

    修改输出的内容

--- mtrace-ggj-agent/src/main/java/com/ggj/tester/MTAgent.java
+++ mtrace-ggj-agent/src/main/java/com/ggj/tester/MTAgent.java
@@ -23,14 +23,14 @@ public class MTAgent {
 
         String projectPath = System.getProperty("user.dir");
         String projectName = projectPath.substring(projectPath.lastIndexOf("/") + 1);
+        String traceFile;
         // 重定向输出到指定文件
         if (null == traceFilePath || traceFilePath.length() == 0) {
-            String traceFile = projectPath + "/" + formatDate + "_" + projectName + "_Trace.log";
-            redirectOutPut(traceFile);
+            traceFile = projectPath + "/" + formatDate + "_" + projectName + "_Trace.log";
         } else {
-            String traceFile = traceFilePath + "/" + formatDate + "_" + projectName + "_Trace.log";
-            redirectOutPut(traceFile);
+            traceFile = traceFilePath + "/" + formatDate + "_" + projectName + "_Trace.log";
         }
+        redirectOutPut(traceFile);
 
         //向instrumentation中添加一个类的转换器,用于转换类的行为.
         instrumentation.addTransformer(new ClassTransformer(agentOptions));
